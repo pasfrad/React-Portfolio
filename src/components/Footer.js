389696ab1521d14ee18f7a4929c0e39d156d5d56
Reese Hartnett <juliapoole44@gmail.com>
2022-05-21 18:46:15 -0400

    style project boxes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailIcon from '@mui/icons-material/Mail';
 
 export default function Footer() {
     return (
         <div className='footer'>
-          Footer stuff
+          <GitHubIcon fontSize="large" color="action" id="footerIcon"/>
+          <LinkedInIcon fontSize="large" color="action" id="footerIcon"/>
+          <MailIcon fontSize="large" color="action" id="footerIcon"/>
         </div>
     );
  }
--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,88 +1,85 @@
 import React from 'react';
+import techBlog from './images/techBlog.png'
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Projects() {
   return (
     <div>
-      {/* First row of two projects */}
       <div className="container mt-1">
         <div class="row">
+          <div className="col-md-auto" id="projectCards">
+            <h5 className="cardHeader">
+              <a id="link" href="https://peaceful-reaches-79315.herokuapp.com/">Tech Blog</a>
+              <a href="https://github.com/pasfrad/Tech-blog">
+                <GitHubIcon fontSize="large" color="action" id="gitIcon"/>
+              </a>
+            </h5>
+            <a href="https://github.com/pasfrad/Tech-blog">
+            <img src={techBlog} class="img" alt="tech blog project"></img>
+            </a>
+          </div>
+
+          <div className="col-md-auto" id="projectCards">
+            <h5 className="cardHeader">
+              <a id="link" href="https://peaceful-reaches-79315.herokuapp.com/">Tech Blog</a>
+              <a href="https://github.com/pasfrad/Tech-blog">
+                <GitHubIcon fontSize="large" color="action" id="gitIcon"/>
+              </a>
+            </h5>
+            <a href="https://github.com/pasfrad/Tech-blog">
+            <img src={techBlog} class="img" alt="tech blog project"></img>
+            </a>
+          </div>
+
+          <div className="col-md-auto" id="projectCards">
+            <h5 className="cardHeader">
+              <a id="link" href="https://peaceful-reaches-79315.herokuapp.com/">Tech Blog</a>
+              <a href="https://github.com/pasfrad/Tech-blog">
+                <GitHubIcon fontSize="large" color="action" id="gitIcon"/>
+              </a>
+            </h5>
+            <a href="https://github.com/pasfrad/Tech-blog">
+            <img src={techBlog} class="img" alt="tech blog project"></img>
+            </a>
+          </div>
+
+
+          <div className="col-md-auto" id="projectCards">
+            <h5 className="cardHeader">
+              <a id="link" href="https://peaceful-reaches-79315.herokuapp.com/">Tech Blog</a>
+              <a href="https://github.com/pasfrad/Tech-blog">
+                <GitHubIcon fontSize="large" color="action" id="gitIcon"/>
+              </a>
+            </h5>
+            <a href="https://github.com/pasfrad/Tech-blog">
+            <img src={techBlog} class="img" alt="tech blog project"></img>
+            </a>
+          </div>
+
+          <div className="col-md-auto" id="projectCards">
+            <h5 className="cardHeader">
+              <a id="link" href="https://peaceful-reaches-79315.herokuapp.com/">Tech Blog</a>
+              <a href="https://github.com/pasfrad/Tech-blog">
+                <GitHubIcon fontSize="large" color="action" id="gitIcon"/>
+              </a>
+            </h5>
+            <a href="https://github.com/pasfrad/Tech-blog">
+            <img src={techBlog} class="img" alt="tech blog project"></img>
+            </a>
+          </div>
+
+          <div className="col-md-auto" id="projectCards">
+            <h5 className="cardHeader">
+              <a id="link" href="https://peaceful-reaches-79315.herokuapp.com/">Tech Blog</a>
+              <a href="https://github.com/pasfrad/Tech-blog">
+                <GitHubIcon fontSize="large" color="action" id="gitIcon"/>
+              </a>
+            </h5>
+            <a href="https://github.com/pasfrad/Tech-blog">
+            <img src={techBlog} class="img" alt="tech blog project"></img>
+            </a>
+          </div>
           
-          <div className="col-md-auto" id="projectCards">
-            <a href="https://peaceful-reaches-79315.herokuapp.com/">
-              placeholder
-              {/* <img src="./assets/techblog.png" class="card-img-top" alt="Picture of tech blog project"> */}
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">Tech Blog</h5>
-              <p className="card-text">
-                <a class="link" href="https://github.com/pasfrad/Tech-blog">View Github Repository</a>
-              </p>
-            </div>
-          </div>
-
-          <div className="col-md-auto" id="projectCards">
-            <a href="https://mighty-citadel-27783.herokuapp.com/">
-              placeholder
-              {/* <img src="./assets/JATE.png" class="card-img-top" alt="Picture of JATE project"> */}
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">Just Another Text Editor</h5>
-              <p className="card-text">
-                <a class="link" href="https://github.com/pasfrad/Just-Another-Text-Editor">View Github Repository</a>
-              </p>
-            </div>
-          </div>
-
-          <div className="col-md-auto" id="projectCards">
-            <a href="https://mighty-citadel-27783.herokuapp.com/">
-              placeholder
-              {/* <img src="./assets/JATE.png" class="card-img-top" alt="Picture of JATE project"> */}
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">Just Another Text Editor</h5>
-              <p className="card-text">
-                <a class="link" href="https://github.com/pasfrad/Just-Another-Text-Editor">View Github Repository</a>
-              </p>
-            </div>
-          </div>
-
-        
-          <div className="col-md-auto"  id="projectCards">
-            <a href="https://mighty-citadel-27783.herokuapp.com/">
-              placeholder
-              {/* <img src="./assets/JATE.png" class="card-img-top" alt="Picture of JATE project"> */}
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">Just Another Text Editor</h5>
-              <p className="card-text">
-                <a class="link" href="https://github.com/pasfrad/Just-Another-Text-Editor">View Github Repository</a>
-              </p>
-            </div>
-          </div>
-          <div className="col-md-auto" id="projectCards">
-            <a href="https://mighty-citadel-27783.herokuapp.com/">
-              placeholder
-              {/* <img src="./assets/JATE.png" class="card-img-top" alt="Picture of JATE project"> */}
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">Just Another Text Editor</h5>
-              <p className="card-text">
-                <a class="link" href="https://github.com/pasfrad/Just-Another-Text-Editor">View Github Repository</a>
-              </p>
-            </div>
-          </div>
-          <div className="col-md-auto" id="projectCards">
-            <a href="https://mighty-citadel-27783.herokuapp.com/">
-              placeholder
-              {/* <img src="./assets/JATE.png" class="card-img-top" alt="Picture of JATE project"> */}
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">Just Another Text Editor</h5>
-              <p className="card-text">
-                <a class="link" href="https://github.com/pasfrad/Just-Another-Text-Editor">View Github Repository</a>
-              </p>
-            </div>
-          </div>
         </div>
         <div id="padder"></div>
       </div>
